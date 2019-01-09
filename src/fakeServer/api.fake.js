b9442142921a842ad7701376deffba4dfe739bce
gush <itcstudio@gmail.com>
2019-01-09 16:00:54 +0200

    Generalized fake server maker

--- src/fakeServer/api.fake.js
+++ src/fakeServer/api.fake.js
@@ -1,7 +1,30 @@
-import createList from './services.fake';
-
-export const servicesAPI = new Promise((resolve, reject ) => {
-	setTimeout(() => {
-		resolve( { json: createList(), ok: true } );
-	}, 1500);
+export const callAPI = ( content ) => new Promise((resolve, reject ) => {
+	setTimeout((content) => {
+		resolve( { json: content, ok: true } );
+	}, 1500, content);
 });
+
+/*
+use example:
+
+const content = dataFaker(3, 2, {
+    id: 'id',
+    head: 'header',
+    sub: 'header',
+    body: 'text'
+})
+
+callAPI(content)
+    .then( response => {
+        if ( response.ok ) {
+            return response.json
+        } else {
+            throw new Error('Something went wrong ...');
+        }
+    })
+    .then(
+        data => console.log(data)
+    )
+    .catch( error => console.log(error));
+
+*/
